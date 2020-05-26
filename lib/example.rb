30de30b6b71d616957024555d107767c38c59e0a
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-26 13:59:07 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,18 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+ pharse = "Hello World" 
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
